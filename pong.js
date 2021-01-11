3bdb5717dc695f7054bab2bbb96c418228964894
Justin Foster <jfoster91@gmail.com>
2021-01-11 14:40:29 -0700

    Both paddles move

---
+++ pong.js
@@ -0,0 +1,19 @@
+// let ballX = 80;
+// let ballY = 80;
+
+// rightPaddleX
+// rightPaddleY
+// let leftPaddleX = 30;
+// let leftPaddleY = this.cameras.main.centerY;
+
+// function moveHorizontal(){
+//   return ballX+=2;
+// }
+
+// function moveVertical(y){
+//   return y+=5;
+// }
+
+// function movePaddle(paddle, x, y) {
+//   paddle.setPosition(x, y+=5)
+// }
